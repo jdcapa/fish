1aa64cfbde4f925a64fe6dc6b19b5afb8485a921
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-06-30 15:41:27 +0200

    Hyperlink DOIs to preferred resolver

--- functions/doi.fish
+++ functions/doi.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 function doi
-    curl -sLH "Accept: text/bibliography; style=bibtex" http://dx.doi.org/$argv | sed -r 's/[a-Z]*=/\n    &/g' | sed 's/}}/}\n}\n/g'
+    curl -sLH "Accept: text/bibliography; style=bibtex" https://doi.org/$argv | sed -r 's/[a-Z]*=/\n    &/g' | sed 's/}}/}\n}\n/g'
 end
